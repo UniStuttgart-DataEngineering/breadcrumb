62b5fa0059f502f1959ef816b2bf8dea3519013c
Ralf Diestelkaemper <ralf.diestelkaemper@ipvs.uni-stuttgart.de>
2020-08-09 14:45:51 +0200

    minor changes

--- src/main/external_resources/Scripts/run-tests.sh
+++ src/main/external_resources/Scripts/run-tests.sh
@@ -3,8 +3,7 @@ repetitions="1"
 testMask="127"
 twitterPath="/user/hadoop/diesterf/data/twitter/logs/"
 dblpPath="/user/hadoop/diesterf/data/dblp/json/big/"
-provQuery="none"
-warmup="true"
+warmup="false"
 
 for testSuite in "dblp"; do
     if [ $testSuite = "twitter" ]; then
@@ -12,7 +11,7 @@ for testSuite in "dblp"; do
     else
         dataPath=$dblpPath
     fi
-    for reference in "true" "false"; do
+    for reference in "false"; do
         for size in "100"; do
             for iteration in "1"; do
                 ./submit.sh $testSuite $reference $size $repetitions $warmup $testMask $dataPath
