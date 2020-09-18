43f24765e803cd8867eaf1951836a7051b0167ed
shek21 <slee195@hawk.iit.edu>
2020-09-18 09:53:06 -0400

    dblp5

--- src/main/external_resources/Scripts/run-tests.sh
+++ src/main/external_resources/Scripts/run-tests.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 repetitions="3"
-testMask="127"
+testMask="1"
 twitterPath="/user/hadoop/diesterf/data/twitter/logs/"
 dblpPath="/user/hadoop/diesterf/data/dblp/json/big/"
 warmup="true"
 saSize="1"
 
-for testSuite in "dblp" "twitter"; do
+for testSuite in "dblp"; do
     if [ $testSuite = "twitter" ]; then
         dataPath=$twitterPath
     else
         dataPath=$dblpPath
     fi
-    for reference in "0" "2" "3"; do
-        for size in "100" "200" "300" "400" "500"; do
+    for reference in "3"; do
+        for size in "400" "500"; do
             for iteration in "1"; do
                 ./submit.sh $testSuite $reference $size $repetitions $warmup $testMask $dataPath $saSize
             done
