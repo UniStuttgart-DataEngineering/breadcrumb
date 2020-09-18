e98022ff9790d04218e085d03a1b1db31c6b9628
shek21 <slee195@hawk.iit.edu>
2020-09-18 11:32:59 -0400

    more

--- src/main/external_resources/Scripts/copy_libs_to_master.sh
+++ src/main/external_resources/Scripts/copy_libs_to_master.sh
@@ -6,7 +6,7 @@ export PROV_LIB=${CONF_BASE_DIR}/target/nested-why-not-0.1-SNAPSHOT.jar
 
 scp -i /Users/shek21/.ssh/id_rsa ${PROV_LIB} hadoop@bigmaster:/data/diesterf/libs
 scp -i /Users/shek21/.ssh/id_rsa ${RESOURCE_DIR}/Scripts/submit.sh hadoop@bigmaster:/data/diesterf/libs
-scp -i /Users/shek21/.ssh/id_rsa ${RESOURCE_DIR}/Scripts/run-tests.sh hadoop@bigmaster:/data/diesterf/libs
+scp -i /Users/shek21/.ssh/id_rsa ${RESOURCE_DIR}/Scripts/run-tests* hadoop@bigmaster:/data/diesterf/libs
 scp -i /Users/shek21/.ssh/id_rsa ${RESOURCE_DIR}/Scripts/get_results.sh hadoop@bigmaster:/data/diesterf/libs
 
 echo "done"
