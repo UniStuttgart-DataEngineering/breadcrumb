a2c0b168fa74ca9b575e04ac06ba5efd82a2c517
Ralf Diestelkaemper <ralf.diestelkaemper@ipvs.uni-stuttgart.de>
2021-01-20 16:18:39 +0100

    prepared for rerun

--- src/main/scala/de/uni_stuttgart/ipvs/provenance/evaluation/tpch/NestedOrdersAlternatives.scala
+++ src/main/scala/de/uni_stuttgart/ipvs/provenance/evaluation/tpch/NestedOrdersAlternatives.scala
@@ -4,4 +4,20 @@ import de.uni_stuttgart.ipvs.provenance.schema_alternatives.PrimarySchemaSubsetT
 object NestedOrdersAlternatives extends LineItemAlternatives {
   //override def createAllAlternatives(primarySchemaSubsetTree: PrimarySchemaSubsetTree): PrimarySchemaSubsetTree = primarySchemaSubsetTree
 
+  def createAlternativesWithOrdersWith1Permutations(primarySchemaSubsetTree: PrimarySchemaSubsetTree, attributeAlternativeSetOrder: Seq[String], attributeAlternativeSet1: Seq[String], attributeAlternativeSet2: Seq[String]): PrimarySchemaSubsetTree = {
+    val altPerms0 = attributeAlternativeSetOrder.permutations.toList
+    val altPerms1 = attributeAlternativeSet1.permutations.toList
+    val altPerms2 = attributeAlternativeSet2.map { x => List(x)}.toList
+    val totalAlternativesWithoutOrders = altPerms1.size * altPerms2.size
+    val totalAlternatives = 2*totalAlternativesWithoutOrders - 1
+    val allPerms : List[List[String]] = altPerms1 cross altPerms2
+    val original : List[String] = allPerms.head
+    val primaryTree = createAlternatives(primarySchemaSubsetTree, totalAlternatives)
+    for ((tree, combination) <- primaryTree.alternatives zip allPerms.tail) {
+      createAlternative(tree, original, combination)
+    }
+    primaryTree
+  }
+
+
 }
