1a4f9a40d6860e15018b7a8a7ce96d00d6ec50f7
shek21 <slee195@hawk.iit.edu>
2021-01-23 23:23:13 -0500

    tpch10

--- src/main/external_resources/Scripts/run-tests.sh
+++ src/main/external_resources/Scripts/run-tests.sh
@@ -18,7 +18,7 @@ for saSize in "1"; do #"1" "2" "3" "4"
           dataPath=$tpchPath
       fi
       for reference in "4"; do #"3" "5"
-          for size in "100"; do
+          for size in "100" "200"; do
                 ./submit.sh $testSuite $reference $size $repetitions $warmup $testMask $dataPath $saSize
           done
       done
--- src/main/scala/de/uni_stuttgart/ipvs/provenance/evaluation/tpch/TPCHSuite.scala
+++ src/main/scala/de/uni_stuttgart/ipvs/provenance/evaluation/tpch/TPCHSuite.scala
@@ -18,14 +18,14 @@ class TPCHSuite (spark: SparkSession, testConfiguration: TestConfiguration) exte
 
   // Queries for flat
   //addScenario(new TPCHScenario00(spark, testConfiguration))
-  addScenario(new TPCHScenario000(spark, testConfiguration))
+//  addScenario(new TPCHScenario000(spark, testConfiguration))
 
   //addScenario(new TPCHScenario03(spark, testConfiguration))
   //addScenario(new TPCHScenario04(spark, testConfiguration))
-  //addScenario(new TPCHScenario10(spark, testConfiguration))
+  addScenario(new TPCHScenario10(spark, testConfiguration))
   //addScenario(new TPCHScenario13(spark, testConfiguration))
 
-  //addScenario(new TPCHScenario06(spark, testConfiguration))
+//  addScenario(new TPCHScenario06(spark, testConfiguration))
   //addScenario(new TPCHScenario01(spark, testConfiguration))
 
   // Queries for nesting
