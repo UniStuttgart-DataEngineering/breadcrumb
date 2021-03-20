0db27578a4bc9634159368b62fb82861881e583d
Ralf Diestelkaemper <ralf.diestelkaemper@ipvs.uni-stuttgart.de>
2021-03-20 15:20:53 +0100

    removed deprecated shell scripts

--- src/main/external_resources/Scripts/get_results.sh
+++
@@ -1,4 +0,0 @@
-rm /data/diesterf/measurements/aggregated_runs.csv
-hdfs dfs -copyToLocal /user/hadoop/diesterf/measurements/aggregated_runs.csv /data/diesterf/measurements/aggregated_runs.csv
-rm /data/diesterf/measurements/runs.csv
-hdfs dfs -copyToLocal /user/hadoop/diesterf/measurements/runs.csv /data/diesterf/measurements/runs.csv
--- src/main/external_resources/Scripts/submit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export LIB_BASE_DIR=/data/diesterf/libs
-echo $1 $2 $3 $4 $5 $6 $7
-spark-submit --deploy-mode cluster  --jars ${LIB_BASE_DIR}/nested-why-not-0.1-SNAPSHOT.jar --num-executors 48 --executor-memory 16G --executor-cores 2  --class de.uni_stuttgart.ipvs.provenance.evaluation.TestExecution  ${LIB_BASE_DIR}/nested-why-not-0.1-SNAPSHOT.jar  $1 $2 $3 $4 $5 $6 $7
